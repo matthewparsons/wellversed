e6b91a74541b3dab21d4125a06b5c98385d1e48f
matthewparsons <matthew.r.parsons@gmail.com>
2013-08-28 11:32:05 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wellversed::Application.config.secret_token = '0c2340cdad234a674482e5f1bb340c503398cd5802be707bcfd21fcd78e9372ea576b689dad3485df87a66a4095a81c679110dcc7ae7dba539feb0faa0c6bc86'
