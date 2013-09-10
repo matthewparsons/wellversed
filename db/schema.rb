7141185fc7fdb37eb4523e1b6dd453ead61c1ace
matthewparsons <matthew.r.parsons@gmail.com>
2013-09-10 15:43:33 +0100

    Make a basic User model (including secure password)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130830095600) do
+ActiveRecord::Schema.define(:version => 20130910140143) do
 
   create_table "pieces", :force => true do |t|
     t.string   "title"
@@ -26,8 +26,11 @@ ActiveRecord::Schema.define(:version => 20130830095600) do
     t.string   "email"
     t.string   "firstname"
     t.string   "surname"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+    t.string   "password_digest"
   end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
 end
