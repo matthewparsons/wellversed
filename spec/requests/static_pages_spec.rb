e60a60c002249cf36f46436c30c0e1350a533022
matthewparsons <matthew.r.parsons@gmail.com>
2013-09-13 14:24:28 +0100

    Finish user edit, update, index, and destroy actions

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -51,7 +51,7 @@ describe "Static pages" do
     click_link "Home"
     click_link "Sign up now!"
     page.should have_selector 'title', text: full_title('Sign up')
-    click_link "Rhymember"
+    click_link "rhymember"
      page.should have_selector 'title', text: full_title('')
   end
 
